e815f2b0b7fd93d628048063e76a2bf7a49708ca
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-20 22:32:49 +0530

    Add files via upload

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,15 +1,17 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = function (req, res, next) {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
 
-  if (!token) return res.status(401).json({ message: "No token" });
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch {
-    res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
