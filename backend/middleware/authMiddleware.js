196ec1088e224dd16789fc27ae904253abd8b018
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-21 00:55:16 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,19 @@
 
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ msg: "No token" });
+
+  try {
+    token = token.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Invalid token" });
+  }
+};
+
+module.exports = protect;
