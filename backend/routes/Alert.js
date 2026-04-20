319182b103c9884e90a2bd0b3d24c2316cf2a708
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-21 01:03:11 +0530

    Delete backend/routes/Alert.js

--- backend/routes/Alert.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Alert", {
-  userId: String,
-  service: String,
-  amount: Number,
-  expiry: Date,
-  frequency: String
-});
