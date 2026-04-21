9b0c65358cc8ed91d7cd15c03bc8f21c63eef83d
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-21 17:14:00 +0530

    Add files via upload

--- backend/utlis/jwt.js
+++ backend/utlis/jwt.js
@@ -1,9 +1,7 @@
-const jwt = require("jsonwebtoken");
+import jwt from "jsonwebtoken";
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+export const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
 };
-
-module.exports = generateToken;
