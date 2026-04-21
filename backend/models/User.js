993079ee78a7403f7707eb32f71096b545b0cf78
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-21 15:52:19 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.model("User", {
-  name: String,
-  email: String,
-  password: String
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    phone: String,
+    isVerified: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
