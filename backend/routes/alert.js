50b8b6d53bb2dbc0ab78fe8f29ecd9e96256e528
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-29 23:53:32 +0530

    Update alert.js

--- backend/routes/alert.js
+++ backend/routes/alert.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Alert = require("../models/Alert");
-const auth = require("../middleware/auth");
+const auth = require("../middleware/authMiddleware");
 const sendEmail = require("../services/mailer");
 
 // ✅ CREATE NODE
