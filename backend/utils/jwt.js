c46863b0a59b33d120f36342a511cd8adddcdeaa
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-30 00:15:49 +0530

    Update jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,11 +1,17 @@
-import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 
-export const generateToken = (id) => {
+const generateToken = (id) => {
     if (!process.env.JWT_SECRET) {
         throw new Error("JWT_SECRET is missing in environment variables");
     }
 
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-    });
+    return jwt.sign(
+        { id },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d",
+        }
+    );
 };
+
+module.exports = generateToken;
