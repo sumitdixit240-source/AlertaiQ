d7945ecefacf3c0f7ac7722c0a376f2343c5746e
sumitdixit240-source <sumitdixit240@gmail.com>
2026-04-30 00:17:01 +0530

    Update generateOTP.js

--- backend/utils/generateOTP.js
+++ backend/utils/generateOTP.js
@@ -1,7 +1,14 @@
 const crypto = require("crypto");
 
-const generateOTP = () => {
-  return crypto.randomInt(100000, 1000000).toString();
+const generateOTP = (length = 6) => {
+  if (length < 4 || length > 10) {
+    throw new Error("OTP length must be between 4 and 10");
+  }
+
+  const max = Math.pow(10, length);
+  const min = Math.pow(10, length - 1);
+
+  return crypto.randomInt(min, max).toString();
 };
 
 module.exports = generateOTP;
